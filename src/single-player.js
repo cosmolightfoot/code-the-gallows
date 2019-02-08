20a97a1a11fdc107dfe53fd5e5dbe82d0af3d601
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-08 15:03:32 -0800

    created hangman css layout

---
+++ src/single-player.js
@@ -0,0 +1,36 @@
+const gallowsNode = document.getElementById('gallows');
+const buildGallows = [
+    {
+        name: 'head',
+        imgSrc: ''
+    },
+    {
+        name: 'torso',
+        imgSrc: ''
+    },
+    {
+        name: 'left-arm',
+        imgSrc: ''
+    },
+    {
+        name: 'right-arm',
+        imgSrc: ''
+    },
+    {
+        name: 'left-leg',
+        imgSrc:''
+    },
+    {
+        name: 'right-leg',
+        imgSrc:''
+    }
+];
+
+for(let index = 0; index < buildGallows.length; index++) {
+    const penaltyBlock = document.createElement('div');
+    penaltyBlock.classList.add('penalty-block');
+    penaltyBlock.textContent = buildGallows[index].name;
+    console.log(penaltyBlock);
+    penaltyBlock.id = buildGallows[index].name;
+    gallowsNode.appendChild(penaltyBlock);
+}
