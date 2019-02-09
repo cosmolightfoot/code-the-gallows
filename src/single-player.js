34e6768c67ae7186916bbe64da9fae617c30461f
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-09 14:51:16 -0800

    added wordlist

--- src/single-player.js
+++ src/single-player.js
@@ -40,18 +40,15 @@ const buildGallows = [
         imgSrc:'../assets/tentacles-1.png',
         imgWidth: 225
     },
-    {
-        name: 'frame', 
-        imgSrc: '../assets/picture-frame-1.png',
-        imgWidth: 950
-    }
 ];
 
+//builds gallows images
 for(let index = 0; index < buildGallows.length; index++) {
     const penaltyBlock = document.createElement('div');
     const blockImage = document.createElement('img');
     blockImage.width = buildGallows[index].imgWidth;
     blockImage.src = buildGallows[index].imgSrc;
+    // blockImage.classList.add('hidden');
     penaltyBlock.classList.add('penalty-block');
     penaltyBlock.id = buildGallows[index].name;
     penaltyBlock.appendChild(blockImage);
@@ -65,9 +62,6 @@ const rowThreeNode = document.getElementById('row-three');
 
 const buildLetterButtons = [
     {
-        letter: 'A'
-    },
-    {
         letter: 'B'
     },
     {
@@ -77,9 +71,6 @@ const buildLetterButtons = [
         letter: 'D'
     },
     {
-        letter: 'E'
-    },
-    {
         letter: 'F'
     },
     {
@@ -87,9 +78,6 @@ const buildLetterButtons = [
     },
     {
         letter: 'H'
-    },
-    {
-        letter: 'I'
     },
     {
         letter: 'J'
@@ -107,7 +95,22 @@ const buildLetterButtons = [
         letter: 'N'
     },
     {
+        letter: 'A'
+    },
+    {
+        letter: 'E'
+    },
+    {
+        letter: 'I'
+    },
+    {
         letter: 'O'
+    },
+    {
+        letter: 'U'
+    },
+    {
+        letter: 'Y'
     },
     {
         letter: 'P'
@@ -125,9 +128,6 @@ const buildLetterButtons = [
         letter: 'T'
     },
     {
-        letter: 'U'
-    },
-    {
         letter: 'V'
     },
     {
@@ -137,34 +137,27 @@ const buildLetterButtons = [
         letter: 'X'
     },
     {
-        letter: 'Y'
-    },
-    {
         letter: 'Z'
     }
 ];
 
-// for(let index = 0; index < buildLetterButtons.length; index++) {
-//     const letterButton = document.createElement('button');
-//     letterButton.classList.add('letter-button');
-//     letterButton.textContent = buildLetterButtons[index].letter;
+//builds letter buttons
 
-//     letterButtonsNode.appendChild(letterButton);
-// }
-console.log(rowOneNode);
+//row1
 for(let index = 0; index < 10; index++) {
     const letterButton = document.createElement('button');
     letterButton.classList.add('letter-button');
     letterButton.textContent = buildLetterButtons[index].letter;
-
     rowOneNode.appendChild(letterButton);
 }
+//row2
 for(let index = 10; index < 18; index++) {
     const letterButton = document.createElement('button');
     letterButton.classList.add('letter-button');
     letterButton.textContent = buildLetterButtons[index].letter;
 
     rowTwoNode.appendChild(letterButton);
+//row3
 }
 for(let index = 18; index < 26; index++) {
     const letterButton = document.createElement('button');
@@ -173,3 +166,11 @@ for(let index = 18; index < 26; index++) {
 
     rowThreeNode.appendChild(letterButton);
 }
+
+const wordList = ['awkward', 'bagpipes', 'banjo', 'bungler', 'croquet', 'crypt', 'dwarves', 'fervid', 'fishhook', 'fjord', 'gazebo', 'gypsy', 'haiku', 'haphazard', 'hyphen', 'ivory', 'jazzy', 'jiffy', 'jinx', 'jukebox', 'kayak', 'kiosk', 'klutz', 'memento', 'mystify', 'numbskull', 'ostracize', 'oxygen', 'pajama', 'phlegm', 'pixel', 'polka', 'quad', 'quip', 'rhythmic', 'rogue', 'sphinx', 'squawk', 'swivel', 'toady', 'twelfth', 'unzip', 'waxy', 'wildebeest', 'yacht', 'zealous', 'zigzag', 'zippy', 'zombie', 'abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes', 'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar', 'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'caliph', 'cobweb', 'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex', 'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord', 'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby', 'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 'gnarly', 'gnostic', 'gossip', 'grogginess', 'haiku', 'haphazard', 'hyphen', 'iatrogenic', 'icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jaundice', 'jawbreaker', 'jaywalk', 'jazziest', 'jazzy', 'jelly', 'jigsaw', 'jinx', 'jiujitsu', 'jockey', 'jogging', 'joking', 'jovial', 'joyful', 'juicy', 'jukebox', 'jumbo', 'kayak', 'kazoo', 'keyhole', 'khaki', 'kilobyte', 'kiosk', 'kitsch', 'kiwifruit', 'klutz', 'knapsack', 'larynx', 'lengths', 'lucky', 'luxury', 'lymph', 'marquis', 'matrix', 'megahertz', 'microwave', 'mnemonic', 'mystify', 'naphtha', 'nightclub', 'nowadays', 'numbskull', 'nymph', 'onyx', 'ovary', 'oxidize', 'oxygen', 'pajama', 'peekaboo', 'phlegm', 'pixel', 'pizazz', 'pneumonia', 'polka', 'pshaw', 'psyche', 'puppy', 'puzzling', 'quartz', 'queue', 'quips', 'quixotic', 'quiz', 'quizzes', 'quorum', 'razzmatazz', 'rhubarb', 'rhythm', 'rickshaw', 'schnapps', 'scratch', 'shiv', 'snazzy', 'sphinx', 'spritz', 'squawk', 'staff', 'strength', 'strengths', 'stretch', '', 'stronghold', 'stymied', 'subway', 'swivel', 'syndrome', 'thriftless', 'thumbscrew', 'topaz', 'transcript', 'transgress', 'transplant', 'triphthong', 'twelfth', 'twelfths', 'unknown', 'unworthy', 'unzip', 'uptown', 'vaporize', 'vixen', 'vodka', 'voodoo', 'vortex', 'voyeurism', 'walkway', 'waltz', 'wave', 'wavy', 'waxy', 'wellspring', 'wheezy', 'whiskey', 'whizzing', 'whomever', 'wimpy', 'witchcraft', 'wizard', 'woozy', 'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'yippee', 'yoked', 'youthful', 'yummy', 'zephyr', 'zigzag', 'zigzagging', 'zilch', 'zipper', 'zodiac'];
+
+let ranNum = Math.floor(Math.random() * wordList.length);
+let objectiveWord = wordList[ranNum];
+console.log(objectiveWord); 
+
+
